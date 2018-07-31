df2b328243e8eceb0f81264c4cf31ae0e0ef7482
Yuan Sun <Yuan.Sun@nakisa.com>
2018-07-31 17:00:35 -0400

    Not finish yet -- Spring boot app

--- src/main/java/com/acquisio/app/controller/EmployeeController.java
+++ src/main/java/com/acquisio/app/controller/EmployeeController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Map;
@@ -22,6 +25,32 @@ public class EmployeeController {
     @RequestMapping("/employees")
     @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Employee> listEmployees() throws SQLException {
-        return employeeRepository.findAll();
+        return employeeRepository.findAll().stream()
+        .map(employee -> {
+          return md5Hex(employee.getEmail());
+        })
+        .collect(Collectors.toList());
+    }
+    
+    
+    // Copy from Gravatar hash function
+    private static String hex(byte[] array) {
+      StringBuffer sb = new StringBuffer();
+      for (int i = 0; i < array.length; ++i) {
+      sb.append(Integer.toHexString((array[i]
+          & 0xFF) | 0x100).substring(1,3));        
+      }
+      return sb.toString();
+    }
+    
+    private static String md5Hex (String message) {
+      try {
+      MessageDigest md = 
+          MessageDigest.getInstance("MD5");
+      return hex (md.digest(message.getBytes("CP1252")));
+      } catch (NoSuchAlgorithmException e) {
+      } catch (UnsupportedEncodingException e) {
+      }
+      return null;
     }
 }
