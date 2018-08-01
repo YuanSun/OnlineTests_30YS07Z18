41805db0ed94b62363456aa8e9b077dc981e419e
Yuan Sun <Yuan.Sun@nakisa.com>
2018-08-01 09:24:42 -0400

    Finish Spring-boot project

--- src/main/java/com/acquisio/app/controller/EmployeeController.java
+++ src/main/java/com/acquisio/app/controller/EmployeeController.java
@@ -1,57 +1,58 @@
 package com.acquisio.app.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.acquisio.app.domain.Employee;
 import com.acquisio.app.repository.EmployeeRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 @RestController
 public class EmployeeController {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+  @Autowired
+  private EmployeeRepository employeeRepository;
 
-    @RequestMapping("/employees")
-    @CrossOrigin(origins = "http://localhost:4200")
-    public Collection<String> listEmployees() throws SQLException {
-        return employeeRepository.findAll().stream()
-        .map(employee -> {
-          return md5Hex(employee.getEmail());
-        })
-        .collect(Collectors.toCollection(ArrayList::new));
-    }
-    
-    
-    // Copy from Gravatar hash function
-    private static String hex(byte[] array) {
-      StringBuffer sb = new StringBuffer();
-      for (int i = 0; i < array.length; ++i) {
+  @RequestMapping("/employees")
+  @CrossOrigin(origins = "http://localhost:4200")
+  public Collection<Employee> listEmployees() throws SQLException {
+    List<Employee> list = employeeRepository.findAll();
+      list.stream().forEach(emp -> {
+        emp.setEmail(md5Hex(emp.getEmail()));
+      });
+      
+      return list;
+  }
+
+
+  // Copy from Gravatar hash function
+  private static String hex(byte[] array) {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < array.length; ++i) {
       sb.append(Integer.toHexString((array[i]
-          & 0xFF) | 0x100).substring(1,3));        
-      }
-      return sb.toString();
+          & 0xFF) | 0x100).substring(1, 3));
     }
-    
-    private static String md5Hex (String message) {
-      try {
-      MessageDigest md = 
+    return sb.toString();
+  }
+
+  private static String md5Hex(String message) {
+    try {
+      MessageDigest md =
           MessageDigest.getInstance("MD5");
-      return hex (md.digest(message.getBytes("CP1252")));
-      } catch (NoSuchAlgorithmException e) {
-      } catch (UnsupportedEncodingException e) {
-      }
-      return null;
+      return hex(md.digest(message.getBytes("CP1252")));
+    } catch (NoSuchAlgorithmException e) {
+    } catch (UnsupportedEncodingException e) {
     }
+    return null;
+  }
 }
